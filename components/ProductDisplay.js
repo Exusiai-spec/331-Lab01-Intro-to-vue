43c8b1a096a0a02b6a394af2af2307d56e3bc26f
Tenderness <1186602926@qq.com>
2024-10-22 23:09:26 +0800

    before 10.1

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -10,12 +10,20 @@ const productDisplay = {
                     </div>
                 </div>
                 <div class="product-info">
+                <a v-bind:href="producurl">
                     <h1>{{title}}</h1>
+                    </a>
                     <p v-if="inventory > 10">In Stock</p>
                     <p v-else-if="inventory <= 10 && inventory > 0">Almost out of Stock</p>
                     <p v-else>Out of Stock</p>
+                    <p v-if="onSale">{{ saleMessage }}</p>
+                    <p>Shipping: {{shipping}}</p>
                     <ul>
                         <li v-for="detail in details">{{detail}}</li>
+                    </ul>
+                    <ul>
+                      <p>size:</p>
+                        <li v-for="size in size">{{size}}</li>
                     </ul>
                     <div v-for="(variant,index) in variants" :key="variant.id" @mouseover="updateVariant(index)"
                         class="color-circle" :style="{backgroundColor: variant.color}">
@@ -27,12 +35,31 @@ const productDisplay = {
             </div>
            
         `,
-        setup() {
+        props: {
+            premium: Boolean
+            },
+          setup(props) {
+            const shipping = computed(()=>{
+                if (props.premium){
+                     return 'Free'
+                    } else {
+                        return 30
+                    }
+                   
+                })
+                            
             const product = ref('Boots')
+            const producurl = ref('http://www.camt.cmu.ac.th')
             const brand = ref('SE 331')
+            const onSale = ref(true)
             // const image = ref('./assets/images/socks_green.jpg')
             // const inStock = ref(true)
             const inventory = ref(100)
+            const size = ref([
+                'L',
+                'M',
+                'S'
+            ])
             const details = ref([
                 '50% cotton',
                 '30% wool',
@@ -62,16 +89,29 @@ const productDisplay = {
             function updateImage(variantImage) {
                 image.value = variantImage
             }
+            const saleMessage = computed(() => {
+                if (onSale.value) {
+                    return `${brand.value} ${product.value} is on sale`;
+                }
+                return '';
+            })
             return {
+                product,
+                producurl,
+                brand,
                 title,
+                onSale,
+                saleMessage,
                 image,
                 inStock,
+                size,
                 inventory,
                 details,
                 variants,
                 addToCart,
                 updateImage,
-                updateVariant
+                updateVariant,
+                shipping
             }
         }
     }
--- main.js
+++ main.js
@@ -3,10 +3,16 @@ const { createApp, ref,computed } = Vue
 const app = createApp({
     setup(){
         const cart = ref(0)      
+        const premium = ref(true)   
+        const premiumMessage = computed(() =>{
+            return premium.value ? 'You are a premium member!' : 'You are not a premium member.';
+        });
             
+  
         return {
-            cart
-
+            cart,
+            premium,
+            premiumMessage
         }
     }
   
